d77e21bf68014f58a548b4d6fdc01083f7c290a7
mjalbert13 <mjalbert13@gmail.com>
2019-12-01 15:33:00 -0500

    api routes complete

--- notes-app-api/libs/billing-lib.js
+++ notes-app-api/libs/billing-lib.js
@@ -1,8 +1,4 @@
 export function calculateCost(storage) {
-  const rate = storage <= 10
-    ? 4
-    : storage <= 100
-      ? 2
-      : 1;
+  const rate = storage <= 10 ? 4 : storage <= 100 ? 2 : 1;
   return rate * storage * 100;
 }
